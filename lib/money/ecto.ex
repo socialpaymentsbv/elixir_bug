74c8a2c8ac60c075aa3bd53176cea5dad54df4a0
Tomasz Kowal <tomasz.kowal@clubcollect.com>
2021-06-15 16:48:18 +0200

    Clean the example some more

--- lib/money/ecto.ex
+++
@@ -1,2 +0,0 @@
-defmodule Money.Ecto do
-end
--- test/bug_test.exs
+++ test/bug_test.exs
@@ -2,7 +2,7 @@ defmodule BugTest do
   use ExUnit.Case
   doctest Bug
 
-  test "this equivalent always works" do
+  test "this test fails every couple of runs" do
     for x <- 1..1000 do
       {:ok, m} = Money.cast(%{amount: "10.00", currency: "EUR"})
     end
